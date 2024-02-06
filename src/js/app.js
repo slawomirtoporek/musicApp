2e6afff633d2a7753d5fa0ffb98f5581789d39a2
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-02-06 13:28:47 +0100

    add the initPages and activatePage to the app object

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,57 @@
-import { settings } from './settings.js';
-import HomeSongs from './components/HomeSongs.js';
+import { select, settings, classNames } from './settings.js';
 
 const app = {
+
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+        
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        thisApp.activatePage(id);
+
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    for(let page of thisApp.pages){
+      page.classList.toggle(
+        classNames.pages.active,
+        pageId == page.id
+      );
+    }
+
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
 
   initData: function(){
     const thisApp = this;
@@ -17,7 +67,6 @@ const app = {
       .then(function(parsedResponse){
         thisApp.data.songs = parsedResponse;
         
-        thisApp.displayHomePage();
       });
   },
 
@@ -25,6 +74,7 @@ const app = {
     const thisApp = this;
 
     thisApp.initData();
+    thisApp.initPages();
   }
 };
 
--- src/js/settings.js
+++ src/js/settings.js
@@ -4,3 +4,26 @@ export const settings = {
     songs: 'songs',
   },
 };
+
+export const select = {
+  containerOf: {
+    player: '.player',
+    pages: '#pages',
+    songs: '#home-page',
+    discover: '#discover-page',
+    search: '#search-page',
+    subscribe: '#subscribe',
+  },
+  nav: {
+    links: '.main-nav a',
+  }
+};
+
+export const classNames = {
+  pages: {
+    active: 'active',
+  },
+  nav: {
+    active: 'active',
+  }
+};
