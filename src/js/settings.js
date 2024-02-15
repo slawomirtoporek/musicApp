ffe6deaf92a2edb639c18e1bef1816c8a0bc53fd
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-02-15 22:40:55 +0100

    publishing the project

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,6 +1,6 @@
 export const settings = {
   db: {
-    url: '//localhost:3131',
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
     songs: 'songs',
   },
 };
